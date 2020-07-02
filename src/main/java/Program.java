a06850b7d562fc65714ccfcf30ad92e16b742e05
roman <vip.polovei55@gmail.com>
2020-07-02 23:07:59 +0300

    Json work

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -13,25 +13,29 @@ public class Program {
         ObjectMapper objectMapper = new ObjectMapper();
 
         PropertyConfigurator.configure("log4j.properties");
-
+        //Vector vector
         Point point1 = new Point(1,1);
         Point point2 = new Point(2,2);
-
+        //Vector vector1
         Point point3 = new Point(4,3);
         Point point4 = new Point(5,5);
-
+        //Vector vector4
         Point point = new Point(2, 2);
         Point point5 = new Point(5, 6);
 
-        Vector vector4 = new Vector(point, point5);
+
         Vector vector = new Vector(point1,point2);
         Vector vector1 = new Vector(point3,point4);
+        Vector vector4 = new Vector(point, point5);
 
+        //Vector result of sum 2 vectors
         Vector vector3 = Vector.SumVector(vector,vector1);
         LOGGER.info(vector3.lengthVector());
-
+        //Vector result of diff 2 vectors
         Vector vector2 = Vector.DiffVector(vector,vector1);
+
         LOGGER.info(vector2);
+
         LOGGER.info(vector4.lengthVector());
 
         objectMapper.writeValue(new File("vector.json"), vector2);
